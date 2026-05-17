43e6f2ce173792abf5db96194334403d3c7b872a
hdsdi3g <admin@hd3g.tv>
2026-05-17 15:02:25 +0200

    fixup! Refactor and clean MetadataThesaurus

---
+++ src/front/app/interfaces/mtd-thesaurus-def.ts
@@ -0,0 +1,26 @@
+import { MtdThesaurusDefClassifierSignature } from "../dto/mtd-thesaurus-def-classifier-signature.interface";
+import { MtdThesaurusDefEntrySignature } from "../dto/mtd-thesaurus-def-entry-signature.interface";
+
+/*
+ * This file is part of MyDMAM.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * Copyright (C) Media ex Machina 2026
+ * 
+ */
+export abstract class MtdThesaurusDef {
+
+    public abstract getEntrySignatureByKeyName(keyName:string): MtdThesaurusDefEntrySignature|null;
+
+    public abstract getClassifierSignature(): MtdThesaurusDefClassifierSignature|null;
+
+}
